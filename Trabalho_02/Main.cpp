4a05097f98a2de71423fbc578becae478670fc48
VLFAri <arvaleafilho@hotmail.com>
2026-06-15 22:28:00 -0400

    A3 feito (não sei se a lógica está certa, mas executa)

--- Trabalho_02/Main.cpp
+++ Trabalho_02/Main.cpp
@@ -1,43 +1,30 @@
-#include "Mesh.h"
-#include "HEMeshDecoration.h"
-#include <iostream>
-#include <cassert>
+#include "MeshDecoration.h"
 
 using namespace tcii::cg;
 
-enum { VD, TD, ED };
-
-namespace tcii::cg {
-  ObjectPtr<TriangleMesh> readOBJ(const char* filename);
-}
+enum { VD, TD };
 
 auto
-decorate1(const Mesh& mesh)
+decorate1(const TriangleMesh& mesh)
 {
   using Color = Vec3f;
   using VA = ElementSoA<Color>;
   using TA = ElementSoA<Color>;
-  using EA = ElementSoA<int>;
-  using MD = HEMeshDecoration<VA, TA, EA>;
+  using MD = MeshDecoration<VA, TA>;
 
-  auto md = MD::novo(mesh);
-  auto nv = mesh.vertices().size();
+  auto md = MD::New(mesh);
+  auto nv = mesh.data().vertexCount();
 
   printf("sizeof(md1): %zu\n", sizeof(*md));
   for (decltype(nv) i = 0; i < nv; ++i)
     attributes<VD>(*md).set(i, Color{0, 0, 1});
   set<VD, 0>(*md, 0, Color{1, 1, 0});
 
-  auto nt = mesh.faces().size();
-
+  auto nt = mesh.data().triangleCount();
 
   for (decltype(nt) i = 0; i < nt; ++i)
     attributes<TD>(*md).set(i, Color{0, 1, 0});
   set<TD, 0>(*md, 0, Color{0, 1, 1});
-
-  md->decorarArestasDeBorda();
-
-
   return md;
 }
 
@@ -47,11 +34,10 @@ decorate2(OtherMD* other)
 {
   using Color = Vec3f;
   using VA = ElementSoA<Color, Vec3f>;
-  using MD = HEMeshDecoration<VA, typename OtherMD::TA, typename OtherMD::EA>;
+  using MD = MeshDecoration<VA, typename OtherMD::TA>;
 
-  auto md = MD::novo(other);
-  auto nv = md->mesh()->vertices().size();
-
+  auto md = MD::New(other);
+  auto nv = md->mesh()->data().vertexCount();
 
   printf("sizeof(md2): %zu\n", sizeof(*md));
   for (decltype(nv) i = 0; i < nv; ++i)
@@ -65,10 +51,9 @@ template <typename OtherMD>
 auto
 decorate3(OtherMD* other)
 {
-  using MD = HEMeshDecoration<typename OtherMD::VA, void, void>;
+  using MD = MeshDecoration<typename OtherMD::VA, void>;
 
-  auto md = MD::novo(other);
-
+  auto md = MD::New(other);
 
   printf("sizeof(md3): %zu\n", sizeof(*md));
   set<VD, 1>(*md, 0, Vec3f{3, 3, 3});
@@ -76,15 +61,42 @@ decorate3(OtherMD* other)
 }
 
 void
-test(const Mesh& mesh)
+test()
 {
+  /*
+  struct A
+  {
+    A() {}
+    void f() {}
+  };
+  struct B
+  {
+    B() {}
+    void f() {}
+  };
+  struct C
+  {
+    int c;
+  };
+  struct EMPTY_BASES D: A, B, C
+  {
+    int d;
+  };
+
+  printf("sizeof(A):%llu\n", sizeof(A));
+  printf("sizeof(C):%llu\n", sizeof(C));
+  printf("sizeof(D):%llu\n", sizeof(D));
+  */
   using VA = ElementSoA<Vec3f, Vec3f>;
   using TA = ElementSoA<Vec3f>;
-  using EA = ElementSoA<int>;
-  using DS = DecorationSet<VA, TA, EA>;
-  DS ds1{mesh.vertices().size(), mesh.faces().size(), mesh.edges().size()};
+  using MD = MeshDecoration<VA, TA>;
+  using DS = DecorationSet<VA, TA, void>;
+  DS ds1{3u, 4u, 5u};
   printf("**%zu\n", sizeof ds1);
 
+  auto& a0 = attributes<0>(ds1);
+  auto& a1 = attributes<1>(ds1);
+  //auto& a2 = attributes<2>(ds);
 
   set<0, 1>(ds1, 0, Vec3f{1, 1, 1});
 
@@ -101,34 +113,36 @@ test(const Mesh& mesh)
 int
 main()
 {
-  auto filename = "../../meshes/f-16.obj";
-  auto triangleMesh = readOBJ(filename);
+  //test();
+  /*
+  puts("Press any key to exit...");
+  (void)getchar();
+  return 0;
+  */
 
-  if (!triangleMesh)
+  auto filename = "../../meshes/f-16.obj";
+  auto mesh = readOBJ(filename);
+
+  if (!mesh)
     printf("Could not read '%s'\n", filename);
   else
   {
-    Mesh minhaMesh(*triangleMesh);
+    //mesh->print(filename);
 
-    test(minhaMesh);
-    std::cout << "\n--- Iniciando Execucao do Pipeline A3 ---\n\n";
-
-    auto md1 = decorate1(minhaMesh);
+    auto md1 = decorate1(*mesh);
 
     std::cout << get<VD, 0>(*md1, 0) << '\n';
     std::cout << get<TD, 0>(*md1, 0) << '\n';
-    md1->printDebugArestas();
-
 
     auto md2 = decorate2(md1.get());
 
     std::cout << get<VD, 1>(*md2, 0) << '\n';
     std::cout << get<TD, 0>(*md2, 0) << '\n';
-    std::cout << "Aresta 0 (Flag recuperada em md2): " << get<ED, 0>(*md2, 0) << '\n';
-
+ 
     auto md3 = decorate3(md2.get());
 
     std::cout << get<VD, 1>(*md3, 0) << '\n';
+
   }
   puts("Press any key to exit...");
   (void)getchar();
